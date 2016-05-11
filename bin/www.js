ac18c7559592cc7a9a40da2b84e68121acc89ddb
WebberWu <bird23074035@gmail.com>
2016-05-11 18:16:53 +0800

    add color module

--- bin/www.js
+++ bin/www.js
@@ -1,12 +1,13 @@
 require('../global.js');
 
-const app = require('../app');
-const http = require('http');
+let colors = require('colors');
+let app = require('../app');
+let http = require('http');
 
-const port = process.env.PORT || '5493';
+let port = process.env.PORT || '5493';
 app.set('port', port);
 
-const server = http.createServer(app);
+let server = http.createServer(app);
 
 server.listen(port);
 console.log(`Start Listen Port ${port} and ${NODE_ENV} mode`);
