c4d8a589a0a7ac18152b0b82c4c60510403f55c0
WebberWu <bird23074035@gmail.com>
2016-05-11 12:21:46 +0800

    拆分架構

--- bin/www.js
+++ bin/www.js
@@ -1,14 +1,14 @@
-"use strict"
 
 require('babel-core/register');
+require('../global.js');
 
 const app = require('../app');
 const http = require('http');
 
-let port = process.env.PORT || '5493';
+const port = process.env.PORT || '5493';
 app.set('port', port);
 
-let server = http.createServer(app);
+const server = http.createServer(app);
 
 server.listen(port);
-console.log(`Start Listen Port ${port}`);
+console.log(`Start Listen Port ${port} and ${NODE_ENV} mode`);
