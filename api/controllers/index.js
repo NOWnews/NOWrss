17c3002d016b928a4bb8fff3772905110f66d7dd
WebberWu <bird23074035@gmail.com>
2016-05-23 11:30:09 +0800

    qoo 是為了測試 rss

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,10 +1,15 @@
 let demo = require('./test');
 let qoo = require('./qoo');
+let admin = require('./admin');
+let rssGet = require('./rssGet');
 
 module.exports = (app) => {
 
     app.use('/test', demo);
     app.use('/qoo', qoo);
+    app.use('/admin', admin);
+    app.use('/rss', rssGet);
+
     app.use('/', (req, res, next) => {
         res.send('<h1>Hello World!</h1>');
     });
