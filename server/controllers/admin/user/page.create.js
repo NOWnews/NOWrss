fe4f23c93acf435e1086a54b12b7f77448536045
WebberWu <bird23074035@gmail.com>
2016-05-26 15:53:15 +0800

    user 用 json 的方式產生

--- server/controllers/admin/user/page.create.js
+++ server/controllers/admin/user/page.create.js
@@ -4,5 +4,32 @@ const models = require('../../../../models');
 const libs = require('../../../../libs');
 
 module.exports = (req, res, next) => {
-    return res.render('admin/user/create');
+    let formData = {
+        title: '新增管理者',
+        action: '/admin/user/create',
+        method: 'post',
+        formColumn: [{
+            title: '姓名',
+            name: 'name',
+            type: 'text'
+        },
+        {
+            title: 'E-mail',
+            name: 'email',
+            type: 'text'
+        },
+        {
+            title: '密碼',
+            name: 'password',
+            type: 'password'
+        },
+        {
+            title: '確認密碼',
+            name: 'checkPassword',
+            type: 'password'
+        }]
+    };
+    return res.render('admin/user/create', {
+        formData
+    });
 };
