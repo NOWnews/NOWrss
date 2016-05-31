2b0aff67beeb70857dd8cae6a133c3f4b78416bd
WebberWu <bird23074035@gmail.com>
2016-05-31 12:25:16 +0800

    list 頁面跟 route

--- server/controllers/admin/rss/page.list.js
+++ server/controllers/admin/rss/page.list.js
@@ -3,6 +3,31 @@ const debug = require('debug')('NOWrss:controllers:admin:rss:page.list');
 const models = require('../../../../models');
 const libs = require('../../../../libs');
 
+const _ = require('lodash');
+const moment = require('moment-timezone');
+const co = require('co');
+
 module.exports = (req, res, next) => {
-    return res.render('admin/rss/list');
+    co(function*() {
+
+        let rssList = yield models.rss.find()
+            .where('trashed').equals(false)
+            .lean()
+            .execAsync();
+
+        _.forEach(rssList, (rss)=>{
+            let startDate = moment(rss.startDate)
+                .tz('Asia/Taipei')
+                .format('YYYY/MM/DD');
+            let endDate = moment(rss.endDate)
+                .tz('Asia/Taipei')
+                .format('YYYY/MM/DD');
+            rss.dateRange = `${startDate} - ${endDate}`;
+        });
+
+        return res.render('admin/rss/list', {
+            rssList
+        });
+    })
+    .catch(next);
 };
