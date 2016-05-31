64f6e41bf87024a55709a866848477ca58a8780a
WebberWu <bird23074035@gmail.com>
2016-05-31 14:54:59 +0800

    date format 加到 lib 裡面

--- libs/index.js
+++ libs/index.js
@@ -1,8 +1,10 @@
 
 const rssTemplate = require('./rssTemplate');
 const hashPwd = require('./hashPwd');
+const dateFormat = require('./dateFormat');
 
 module.exports = {
     hashPwd: hashPwd,
+    dateFormat: dateFormat,
     rssTemplate: rssTemplate
 };
