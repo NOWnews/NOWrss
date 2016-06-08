7dcafe51ed98fa43b1fbb86e99941ad0f9505757
WebberWu <bird23074035@gmail.com>
2016-06-08 16:23:33 +0800

    把 localhost DB 加回來

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongodb: process.env.NODE_ENV === 'production' ? 'mongodb://nowproduction:werocks@192.168.10.181,192.168.10.182,192.168.10.183,192.168.10.184/production' : 'mongodb://nowproduction:werocks@mongodb16.nownews.com.tw,mongodb15.nownews.com.tw,mongodb14.nownews.com.tw,mongodb18.nownews.com.tw/production'
+    mongodb: process.env.NODE_ENV === 'production' ? 'mongodb://nowproduction:werocks@192.168.10.181,192.168.10.182,192.168.10.183,192.168.10.184/production' : 'mongodb://nowproduction:werocks@mongodb16.nownews.com.tw,mongodb15.nownews.com.tw,mongodb14.nownews.com.tw,mongodb18.nownews.com.tw/production',
+    localMongodb: 'mongodb://localhost:27017'
 };
