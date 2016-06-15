36045c9c8a16d0d6a94b1afdee66625dd87c719f
WebberWu <bird23074035@gmail.com>
2016-06-15 10:35:44 +0800

    將 line 裡面的 func 拉到 libs 裡面來

--- libs/index.js
+++ libs/index.js
@@ -2,9 +2,17 @@
 const rssTemplate = require('./rssTemplate');
 const hashPwd = require('./hashPwd');
 const dateFormat = require('./dateFormat');
+const getNeedNewsFromMongo = require('./getNeedNewsFromMongo');
 
 module.exports = {
     hashPwd: hashPwd,
     dateFormat: dateFormat,
-    rssTemplate: rssTemplate
+    rssTemplate: rssTemplate,
+    getNeedNewsFromMongo: getNeedNewsFromMongo,
+
+    getAllMainCategory: getAllMainCategory,
+    getNewsByTids: getNewsByTids,
+    getNewsImageFromNodeId: getNewsImageFromNodeId,
+    checkBodyImageIsAuth: checkBodyImageIsAuth,
+    getRefNews: getRefNews
 };
--- server/controllers/rssGet.js
+++ server/controllers/rssGet.js
@@ -1,7 +1,11 @@
 const debug = require('debug')('NOWrss:controllers:rssGet');
 
+
 let express = require('express');
 let router = express.Router();
+
+const moment = require('moment-timezone');
+const libs = require('../../libs');
 
 let models = require('../../models');
 let caches = require('../../caches');
@@ -16,10 +20,16 @@ router.route('/:id')
         let id = mongoose.Types.ObjectId(req.params.id);
 
         co(function*() {
+            let startTime = moment(Date.now()).add(-15, 'm');
+            let endTime = moment(Date.now());
+
+            let news = yield libs.getNeedNewsFromMongo(startTime, endTime);
+
             let rssData = yield models.rss.findOne()
             .where('_id').equals(id)
             .execAsync();
-            res.json(rssData);
+
+            res.json(news);
         }).catch(next);
 
     });
