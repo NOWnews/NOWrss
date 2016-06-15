6641489604e2f0f01c7677ebf54201d4294899be
WebberWu <bird23074035@gmail.com>
2016-06-15 10:47:42 +0800

    將 line/getNewsFromMongo.js 複製一份到 libs/getNeedNewsFromMongo.js

---
+++ libs/getNeedNewsFromMongo.js
@@ -0,0 +1,98 @@
+/*
+ * 帶入時間區間，去跟 mongodb 要新聞資料
+ */
+
+const debug = require('debug')('NOWrss:line:getNewsFromMongo');
+const co = require('co');
+const Promise = require('bluebird');
+const moment = require('moment-timezone');
+const _ = require('lodash');
+const libs = require('../libs');
+
+const getAllMainCategory = libs.getAllMainCategory;
+const getNewsByTids = libs.getNewsByTids;
+const getNewsImageFromNodeId = libs.getNewsImageFromNodeId;
+const checkBodyImageIsAuth = libs.checkBodyImageIsAuth;
+const getRefNews = libs.getRefNews;
+
+module.exports = co.wrap(function*(start, end) {
+
+    if(!start) {
+        return Promise.reject(new Error('要帶入 start 的 epoch 時間'));
+    }
+
+    if(!end) {
+        return Promise.reject(new Error('要帶入 end 的 epoch 時間'));
+    }
+
+    // 轉換成 epoch time
+    let startEpoch = Math.floor(start / 1000);
+    let endEpoch = Math.floor(end / 1000);
+
+    debug('step 0 = %s', '撈取所有 main category');
+    // 去 db 撈取所有 main category(主要分類) 的資料
+    let mainCategories = yield getAllMainCategory();
+
+    // 所有 main category(主要分類) 的 tid
+    let mainTids = _.map(mainCategories, function(category) {
+        return category.tid;
+    });
+    console.log(mainTids,'####4####');
+    // tid 對應中文名稱
+     var mainMappingObject = {};
+    _.forEach(mainCategories, function(category) {
+        mainMappingObject[category.tid] = category.name;
+    });
+    console.log(mainMappingObject, '####4####');
+
+    // debug('mainTids = %j', mainTids);
+    // debug('mainMappingObject = %j', mainMappingObject);
+
+
+    // 用 tid 與時間區間去撈取新聞
+    let allNews = yield getNewsByTids(mainTids, startEpoch, endEpoch);
+    console.log(mainTids, '####4####', startEpoch, endEpoch);
+
+    debug('step 1 = %s', '撈取新聞');
+    debug('總共撈到 %d 則', allNews.length);
+    // 找不到新聞就直接出去了
+    if(allNews.length === 0) {
+        return yield Promise.resolve([]);
+    }
+
+    // 將分類與新聞 mapping
+    _.forEach(allNews, function(news) {
+        news.category = mainMappingObject[news.field_main_category.tid];
+    });
+
+    debug('step 2 = %s', '找尋新聞主圖');
+    // 找尋新聞主圖
+    allNews = yield Promise.map(allNews, function(news) {
+        return getNewsImageFromNodeId(news);
+    })
+    .then(function(newsHaveImage) {
+        return Promise.resolve(newsHaveImage);
+    });
+
+    debug('step 3 = %s', '確認新聞內文圖是否可以外送');
+    // 確認新聞內文圖是否可以外送
+    allNews = yield Promise.map(allNews, function(news) {
+        return checkBodyImageIsAuth(news);
+    })
+    .then(function(checkedNews) {
+        return Promise.resolve(checkedNews);
+    });
+
+    // 找出推薦新聞，先拿掉，有點危險
+    // debug('step 4 = %s', '找出推薦新聞');
+    // allNews = yield Promise.map(allNews, function(news) {
+    //     return getRefNews(news);
+    // })
+    // .then(function(updateRefNews) {
+    //     debug('updateRefNews = %j', updateRefNews);
+    //     return Promise.resolve(updateRefNews);
+    // });
+
+    // debug('allNews = %j', allNews);
+    return yield Promise.resolve(allNews);
+});
