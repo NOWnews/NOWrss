a1601f4ea0f3f3ebaf311a1a1f8cc85f3d8a7592
WebberWu <bird23074035@gmail.com>
2016-06-28 12:35:08 +0800

    修正 DB 名稱

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     mongodb: process.env.NODE_ENV === 'production' ? 'mongodb://nowproduction:werocks@192.168.10.181,192.168.10.182,192.168.10.183,192.168.10.184/production' : 'mongodb://nowproduction:werocks@mongodb16.nownews.com.tw,mongodb15.nownews.com.tw,mongodb14.nownews.com.tw,mongodb18.nownews.com.tw/production',
-    localMongodb: 'mongodb://localhost:27017'
+    adminMongodb: 'mongodb://localhost:27017'
 };
