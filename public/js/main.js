2eeb5ba78fa794e7461c1fe61270868c90d3d65a
WebberWu <bird23074035@gmail.com>
2016-06-29 16:19:16 +0800

    分類全選全不選功能

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,20 @@
-// $(function(){
-//     TODO function
-// });
+$(function() {
+
+    // 分類的全選/全不選
+    var $checkAll = $('.checkAll');
+    checkOrRemoveCheckAll($checkAll);
+    $checkAll.change(function() {
+        var checkboxes = $(this).closest('label').closest('.checkbox').closest('.form-group').find('.checkbox .checkbox-input');
+        checkboxes.prop('checked', $(this).is(':checked'));
+    });
+    $('.checkbox-input').change(function() {
+        checkOrRemoveCheckAll($checkAll);
+    });
+});
+
+function checkOrRemoveCheckAll($checkAll) {
+    var $checkBox = $('.checkbox-input');
+    var $checkBoxIsClick = $('.checkbox-input:checked');
+    var checkedValue = $checkBoxIsClick.length === $checkBox.length;
+    $checkAll.prop('checked', checkedValue);
+}
