a95156ffe5884cf0b2830d2c56c4de833b443a0a
WebberWu <bird23074035@gmail.com>
2016-07-04 09:55:33 +0800

    刪除不必要檔案

--- server/controllers/admin/page.user.js
+++
@@ -1,9 +0,0 @@
-
-
-const debug = require('debug')('NOWrss:controllers:admin:page.home');
-const models = require('../../../models');
-const libs = require('../../../libs');
-
-module.exports = (req, res, next) => {
-    return res.render('admin/user');
-};
