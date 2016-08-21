e30fab328c0ef684e71c5e6a154a3fef97016d1a
Simon Sun <blackghost0118@gmail.com>
2016-08-21 18:55:22 +0800

    這個專案沒有用 babel 所以無法用 import

--- mongodb14/index.js
+++ mongodb14/index.js
@@ -1,12 +1,13 @@
-import co from 'co';
-import Promise from 'bluebird';
-import mongodb from 'mongodb';
 
-import config from '../config';
+const co = require('co');
+const Promise = require('bluebird');
+const mongodb = require('mongodb');
+
+const config = require('../config');
 
 const MongoDB = Promise.promisifyAll(mongodb);
 const MongoClient = Promise.promisifyAll(MongoDB.MongoClient);
 
-let mongodb14 = MongoClient.connectAsync(config.newsMongodb);
+const mongodb14 = MongoClient.connectAsync(config.newsMongodb);
 
 module.exports = mongodb14;
