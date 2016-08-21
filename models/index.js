19d8444e78c31237b35d2e76a7d0d5b3336dd5a1
Simon Sun <blackghost0118@gmail.com>
2016-08-21 19:27:02 +0800

    更新資料

--- models/index.js
+++ models/index.js
@@ -8,6 +8,7 @@ let autoIncrement = require('mongoose-auto-increment');
 Promise.promisifyAll(mongoose);
 
 const dbUrl = `${config.adminMongodb}/rss_production`;
+console.log(dbUrl);
 mongoose.connect(dbUrl);
 const connection = mongoose.connection;
 
