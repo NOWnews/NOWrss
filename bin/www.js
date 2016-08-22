ea502ebe4fae0b77da37d386eb6b99d3f18699f7
Simon Sun <blackghost0118@gmail.com>
2016-08-22 12:38:55 +0800

    加入 https

--- bin/www.js
+++ bin/www.js
@@ -1,13 +1,21 @@
 require('../global.js');
 
-let colors = require('colors');
+let fs = require('fs');
 let app = require('../server');
 let http = require('http');
-
+let https = require('https');
 let port = process.env.PORT || '9453';
-app.set('port', port);
+let httpsPort = process.env.PORT || '9454';
+
+var httpsOptions = {
+    key: fs.readFileSync('./pems/server.key'),
+    cert: fs.readFileSync('./pems/server.pem')
+};
 
 let server = http.createServer(app);
+let httpsServer = https.createServer(httpsOptions, app);
 
 server.listen(port);
+httpsServer.listen(httpsPort);
 console.log(`Start Listen Port ${port} and ${NODE_ENV} mode`);
+console.log(`https Start Listen Port ${httpsPort} and ${NODE_ENV} mode`);
