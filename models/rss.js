b28fdd1e7b38e86cede140140d6f51a9de070358
WebberWu <bird23074035@gmail.com>
2016-08-26 10:30:02 +0800

    加入 templatr facebook 的判斷

--- models/rss.js
+++ models/rss.js
@@ -33,7 +33,7 @@ const schema = new Schema({
     template: {
         type: String,
         default: 'DEFAULT',
-        enum: ['DEFAULT', 'YAHOO']
+        enum: ['DEFAULT', 'YAHOO', 'FACEBOOK']
     },
 
     simplifiedChinese: {
