9990dbfd74efa4cb630dd42918c46f42d149d9fb
WebberWu <bird23074035@gmail.com>
2017-01-09 11:50:20 +0800

    加上判斷確認是否在設定的時間內

--- server/controllers/rssGet.js
+++ server/controllers/rssGet.js
@@ -35,6 +35,16 @@ router.route('/rss/:channelId')
                 return res.render('404');
             }
 
+            // 如果過期
+            let start = moment(rssData.startDate).tz('Asia/Taipei').valueOf();
+            let today = moment().tz('Asia/Taipei').valueOf();
+            let end = moment(rssData.endDate).add(1, 'day').tz('Asia/Taipei').valueOf();
+            if(today >= end || today < start){
+                console.error(`/rss/${req.params.channelId} 此頁面已過期`)
+                res.status(404);
+                return res.render('404');
+            }
+
             let categoryOption = rssData.catogry.split(',');
             let simplifiedChinese = rssData.simplifiedChinese;
 
