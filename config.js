c64f38812cd3726bec56ad490f1c18a941dba244
JaosnHsieh <appleoxxo@gmail.com>
2017-05-15 12:41:58 +0800

    Feat: add babel , global axios

--- config.js
+++ config.js
@@ -20,5 +20,13 @@ module.exports = {
         host: process.env.NODE_ENV === 'production' ? 'localhost' : 'localhost',
         expireSeconds: 3600
      },
+    /*
+     * api server
+     */
+     apiServer:process.env.NODE_ENV === 'production'?'http://localhost:10000':'http://localhost:10000'
+     ,
+     headers: {
+        "X-NOWnews-API": ["NOWnewsIsFeature"]
+    }
 
 };
