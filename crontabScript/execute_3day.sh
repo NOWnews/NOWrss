48d01e4f301931b91c7b7328c5137a36b44fe295
Webber <bird23074035@gmail.com>
2017-08-21 14:50:35 +0800

    ✨ Feat: add path

--- crontabScript/execute_3day.sh
+++ crontabScript/execute_3day.sh
@@ -5,7 +5,8 @@
 # 0 0 */3 * * /{path}/execute_3day.sh
 #
 # log 的路徑會是 /{path}/crontabScript/execute_3day_log.txt
+# mongo script 的路徑會是 /{path}/crontabScript/removeCounts.js
 ##
 
 /bin/date +"====== %Y %m-%d %H:%M ======" >> /home/nodejs_service/NOWrss/crontabScript/execute_3day_log.txt;
-mongo < ./removeCounts.js >> /home/nodejs_service/NOWrss/crontabScript/execute_3day_log.txt;
+mongo < /home/nodejs_service/NOWrss/crontabScript/removeCounts.js >> /home/nodejs_service/NOWrss/crontabScript/execute_3day_log.txt;
