3911bf5113671db38d9c03b14cee3906f56245ed
Webber <bird23074035@gmail.com>
2017-08-21 14:43:03 +0800

    ✨ Feat: update

--- crontabScript/execute_3day.sh
+++ crontabScript/execute_3day.sh
@@ -3,7 +3,8 @@
 ##
 # 輸入 `crontab -e`
 # 0 0 */3 * * /{path}/execute_3day.sh
+#
 ##
 
-/bin/date +"====== %Y %m-%d %H:%M ======" >> ./execute_3day.log;
-mongo < ./removeCounts.js >> ./execute_3day.log
+/bin/date +"====== %Y %m-%d %H:%M ======" >> /home/execute_3day_log.txt;
+mongo < ./removeCounts.js >> ./execute_3day_log.txt;
