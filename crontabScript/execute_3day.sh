f8ef24359b0c634695ddb4ad4ecf9b7e0c52c054
Webber <bird23074035@gmail.com>
2017-08-21 14:47:08 +0800

    ✨ Feat: 加上路徑

--- crontabScript/execute_3day.sh
+++ crontabScript/execute_3day.sh
@@ -4,7 +4,8 @@
 # 輸入 `crontab -e`
 # 0 0 */3 * * /{path}/execute_3day.sh
 #
+# log 的路徑會是 /{path}/crontabScript/execute_3day_log.txt
 ##
 
-/bin/date +"====== %Y %m-%d %H:%M ======" >> /home/execute_3day_log.txt;
-mongo < ./removeCounts.js >> ./execute_3day_log.txt;
+/bin/date +"====== %Y %m-%d %H:%M ======" >> /home/nodejs_service/crontabScript/execute_3day_log.txt;
+mongo < ./removeCounts.js >> /home/nodejs_service/crontabScript/execute_3day_log.txt;
