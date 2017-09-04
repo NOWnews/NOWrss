d0bffc743ab9ac45659371a00265b7b1cc2b80a4
Webber <bird23074035@gmail.com>
2017-09-04 12:10:11 +0800

    🐛 Fix: forwardPath and forwardPathAsync are DEPRECATED and should be replaced with proxyReqPathReso

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,11 +2,21 @@ import rssFacebookGet from './rssFacebookGet';
 import admin from './admin';
 import rssGet from './rssGet';
 
+import { Image } from '../models';
+import proxy from 'express-http-proxy';
+
 module.exports = (app) => {
 
     app.use('/', rssGet);
     app.use('/', rssFacebookGet);
     app.use('/', admin);
+    app.use('/images/:id', proxy('img.nownews.com', {
+        proxyReqPathResolver: async (req, res) => {
+            let image = await Image.findBySn(req.params.id).execAsync();
+            return image.url;
+        },
+        https: true,
+    }));
 
     return (req, res, next) => next();
 };
